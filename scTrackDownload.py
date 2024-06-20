a9b7adf52c603485b65fca971e757f453bc064de
declankra <declankramper@gmail.com>
2024-06-20 08:55:53 -0500

    track_downloads for clarity

--- scTrackDownload.py
+++ scTrackDownload.py
@@ -1,5 +1,5 @@
 # 1. get track url from soundcloud (go to track's page) -> paste url
-# 2. file will be saved in downloads folder in current directory
+# 2. file will be saved in track_downloads folder in current directory
 # 2. click file -> opens in itunes -> enjoy! 
 
 ## ! pip3 install soundcloud-lib
@@ -16,13 +16,13 @@ async def get_track():
 
 
 async def write_track_to_file(track, filename):
-    if not os.path.exists('./downloads'): # creates 'downloads' directory if it doesn't exist
-        os.makedirs('./downloads')
+    if not os.path.exists('./track_downloads'): # creates 'track_downloads' directory if it doesn't exist
+        os.makedirs('./track_downloads')
     with open(filename, 'wb+') as file:
         await track.write_mp3_to(file)
 
 track = asyncio.run(get_track())
-filename = f'./downloads/{track.artist} - {track.title}.mp3'
-asyncio.run(write_track_to_file(track, filename)) # file will be saved in 'downloads' folder in currect directory
+filename = f'./track_downloads/{track.artist} - {track.title}.mp3'
+asyncio.run(write_track_to_file(track, filename)) # file will be saved in 'track_downloads' folder in currect directory
 
 
